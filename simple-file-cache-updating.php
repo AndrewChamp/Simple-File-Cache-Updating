689abf3741c5cbe9c20a32b3d1e0b094a280c42f
Andrew Champ <atmydarkest@gmail.com>
2014-10-15 11:12:35 -0400

    Made it into a class

--- simple-file-cache-updating.php
+++ simple-file-cache-updating.php
@@ -1,7 +1,24 @@
 <?php
 
-	function version($serverPath, $browserPath=''){
-		print (!$browserPath ? $serverPath : $browserPath.basename($serverPath)).'?version='.filemtime($serverPath);
+	class cache{
+
+		private $siteRoot;
+	
+		public function __construct($_siteRoot){
+			$this->siteRoot = $_siteRoot;
+		}
+	
+		public function file($path){
+			return $path.'?version='.filemtime($this->siteRoot.$path);
+		}
+		
 	}
+	
+	
+	$cache = new cache('/home/user/public_html/yourwebsite.com');
 
 ?>
+
+
+Example:
+<link rel="stylesheet" href="<?=$cache->file('/styles/main.css');?>" type="text/css" />
